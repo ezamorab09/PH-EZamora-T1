6a229b5555676bb3cbaf2fb6f328981258b1bbad
EZamora72 <elzamorab@outlook.com>
2024-06-03 12:27:16 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.prueba.v1',
+  appName: 'ph-prueba-1',
+  webDir: 'www'
+};
+
+export default config;
